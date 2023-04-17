7a90bbda367a5df605d6bccf427bd633eba6db22
anabeatrizgoncalves <aanabbeatrizzin@gmail.com>
2023-04-16 22:06:09 -0300

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, AfterViewInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit{
+  title = 'clientes-app';
+
+  ngAfterViewInit(): void {
+    window.addEventListener('DOMContentLoaded', event => {
+      const sidebarToggle = document.body.querySelector('#sidebarToggle');
+      if (sidebarToggle) {
+        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+          document.body.classList.toggle('sb-sidenav-toggled');
+        }
+        sidebarToggle.addEventListener('click', event => {
+          event.preventDefault();
+          document.body.classList.toggle('sb-sidenav-toggled');
+          localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled').toString());
+        });
+      }
+    });
+    
+  }
+  
+}
